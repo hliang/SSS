9d1617ffadec5d3627f2b1663fdcb792368e4a83
H Liang <hliang@ksu>
2013-12-04 00:33:03 -0600

    add methods for convolution and edge detection

--- ExtendedBufferedImage.java
+++ ExtendedBufferedImage.java
@@ -4,9 +4,15 @@
 
 import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.awt.image.Kernel;
+import java.awt.image.ConvolveOp;
+import java.lang.Math;
 
 public class ExtendedBufferedImage{
     private BufferedImage image;
+
+    public static final float[] sobelFilterX = new float[]{ -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+    public static final float[] sobelFilterY = new float[]{ -1, -2, -1, 0, 0, 0, 1, 2, 1 };
 
     public ExtendedBufferedImage(BufferedImage image){
          this.image = image;
@@ -46,14 +52,6 @@ public class ExtendedBufferedImage{
     }
 
 
-    /*
-     * edge detection
-     */
-    public void getEdges(){
-
-    }
-
-
     /* 
      * Convert from grayscale to black and white (rescale intensity values from [0,255] to [0,1]).
      */
@@ -65,6 +63,54 @@ public class ExtendedBufferedImage{
      * resize
      */
     public void resize() {
+
+    }
+
+
+    /*
+     * rotate
+     */
+    public void rotate() {
+
+    }
+
+    /*
+     * convolve with filter
+     */
+    public BufferedImage convolve( float[] filterArray ) {
+        BufferedImage dstImage = null;
+        Kernel kernel = new Kernel(3, 3, filterArray);  // tell the Kernel class that we want this array to be treated as a 3 x 3 matrix.
+        ConvolveOp op = new ConvolveOp(kernel);
+        dstImage = op.filter(image, null);  // when destination image is null, a BufferedImage will be created with the source ColorModel.
+        return dstImage;
+
+    }
+
+    
+    /*
+     * edge detection
+     */
+    public BufferedImage getEdges(){
+        BufferedImage edgeImage = new BufferedImage(image.getWidth(), image.getHeight(),
+                                                    BufferedImage.TYPE_INT_ARGB);
+        BufferedImage edgeX = convolve(sobelFilterX);
+        BufferedImage edgeY = convolve(sobelFilterY);
+
+        for (int x = 0; x < edgeImage.getWidth(); x++) {
+            for (int y = 0; y < edgeImage.getHeight(); y++) {
+                Color color;
+                Color color1 = new Color(edgeX.getRGB(x, y));   // needs java.awt.Color
+                Color color2 = new Color(edgeY.getRGB(x, y));   // needs java.awt.Color
+                int red = (int) (Math.sqrt(Math.pow(color1.getRed(), 2) + Math.pow(color2.getRed(), 2))/1.42);
+                int green = (int) (Math.sqrt(Math.pow(color1.getGreen(), 2) + Math.pow(color2.getGreen(), 2)) / 1.42);
+                int blue = (int) (Math.sqrt(Math.pow(color1.getBlue(), 2) + Math.pow(color2.getBlue(), 2)) / 1.42);
+
+                color = new Color(red, green, blue);
+                int rgb = color.getRGB();
+                edgeImage.setRGB(x, y, rgb);
+            }
+        }
+        return edgeImage;
 
     }
 
--- HoughTransform.java
+++ HoughTransform.java
@@ -48,44 +48,6 @@ import javax.imageio.ImageIO;
  
 public class HoughTransform extends Thread { 
  
-    public static void main(String[] args) throws Exception { 
-        String inFileName = "./testdata/line.png"; 
-        if (args.length >= 1) {
-            inFileName = args[0];
-        }
- 
-        // load the file using Java's imageIO library 
-        BufferedImage image = javax.imageio.ImageIO.read(new File(inFileName)); 
- 
-        // create gray image
-        ExtendedBufferedImage ebimg = new ExtendedBufferedImage(image);
-        BufferedImage grayImage = ebimg.getGrayImage();
-        ImageIO.write(grayImage, "PNG", new File("gray.png"));
-
-        // create a hough transform object with the right dimensions 
-        HoughTransform h = new HoughTransform(image.getWidth(), image.getHeight()); 
- 
-        // add the points from the image (or call the addPoint method separately if your points are not in an image 
-        h.addPoints(image); 
- 
-        // hough transform image
-        BufferedImage htImage = h.getHoughArrayImage();
-        ImageIO.write(htImage, "PNG", new File("ht.png"));
-
-        // get the lines out 
-        Vector<HoughLine> lines = h.getLines(100); 
- 
-        // draw the lines (in red color) back onto the image 
-        for (int j = 0; j < lines.size(); j++) { 
-            HoughLine line = lines.elementAt(j); 
-            line.draw(image, Color.RED.getRGB()); 
-        } 
-
-        // save image
-        ImageIO.write(image, "PNG", new File("output.png"));
-
-    } 
- 
     // The size of the neighbourhood in which to search for other local maxima 
     final int neighbourhoodSize = 4; 
  
@@ -330,4 +292,48 @@ public class HoughTransform extends Thread {
     	return grayImage;
     }
  
+ 
+    public static void main(String[] args) throws Exception { 
+        String inFileName = "./testdata/line.png"; 
+        if (args.length >= 1) {
+            inFileName = args[0];
+        }
+ 
+        // load the file using Java's imageIO library 
+        BufferedImage image = javax.imageio.ImageIO.read(new File(inFileName)); 
+ 
+        // create gray image
+        ExtendedBufferedImage ebimg = new ExtendedBufferedImage(image);
+        BufferedImage grayImage = ebimg.getGrayImage();
+        ImageIO.write(grayImage, "PNG", new File("gray.png"));
+
+        // get edges in gray image
+        ebimg = new ExtendedBufferedImage(grayImage);
+        BufferedImage edgeImage = ebimg.getEdges();
+        ImageIO.write(edgeImage, "PNG", new File("edge.png"));
+
+
+        // create a hough transform object with the right dimensions 
+        HoughTransform h = new HoughTransform(image.getWidth(), image.getHeight()); 
+ 
+        // add the points from the image (or call the addPoint method separately if your points are not in an image 
+        h.addPoints(image); 
+ 
+        // hough transform image
+        BufferedImage htImage = h.getHoughArrayImage();
+        ImageIO.write(htImage, "PNG", new File("ht.png"));
+
+        // get the lines out 
+        Vector<HoughLine> lines = h.getLines(100); 
+ 
+        // draw the lines (in red color) back onto the image 
+        for (int j = 0; j < lines.size(); j++) { 
+            HoughLine line = lines.elementAt(j); 
+            line.draw(image, Color.RED.getRGB()); 
+        } 
+
+        // save image
+        ImageIO.write(image, "PNG", new File("output.png"));
+
+    } 
 } 
